5b58c296a3fc12b74c8e34a3ca38a182eaf1cd17
einarzb <einarzb@gmail.com>
2017-04-03 20:13:00 +0300

    added bootstrap for react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {Navbar, Jumbotron, Button, Grid} from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <Navbar inverse fixedTop>
+         <Grid>
+        <Navbar.Header>
+          <Navbar.Brand>
+          <a href="/">Expenses App </a>
+          </Navbar.Brand>
+          <Navbar.Toggle/>
+          </Navbar.Header>
+          </Grid>
+       </Navbar>
+       <Jumbotron>
+         <Grid>
           <h2>Welcome to React</h2>
-        </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <Button
+            bsStyle="success"
+            bsSize="large"
+            href="http://react-bootstrap.github.io/components.html"
+            target="_blank">
+            View React Bootstrap Docs
+          </Button>
         </p>
+        </Grid>
+      </Jumbotron>
       </div>
     );
   }
